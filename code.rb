7dfcd7d83cfe11db2afb83f348ead992cd1b197f
Jose Geancarlo Bermudez Blandon <geancarlosberm54@gmail.com>
2022-04-29 14:25:13 -0600

    expo

---
+++ code.rb
@@ -0,0 +1,120 @@
+require 'json'
+class Person
+    attr_accessor :name, :last_name, :code, :salary
+    def initialize(name,last_name,code,salary)
+        @name = name
+        @last_name = last_name
+        @code = code
+        @salary = salary
+    end
+end
+
+class Employee < Person
+    #attr_writer :name, last_name
+    #attr_reader :name, last_name
+    attr_accessor :name, :last_name, :code,:salary
+    def initialize(name,last_name,code,salary,number)
+        super(name,last_name,code,salary)
+        @number = number
+    end
+    #def name
+        #@name
+    #end
+    #def name=(name)
+        #@name = name
+    #end
+end
+
+
+class Employer < Person
+    #attr_writer :name, last_name
+    #attr_reader :name, 444last_name
+    attr_accessor :name, :last_name, :code,:salary
+    def initialize(name,last_name,code,salary,email)
+        super(name,last_name,code, salary)
+        @email = email
+    end
+    #def name
+        #@name
+    #end
+    #def name=(name)
+        #@name = name
+    #end
+end
+class Manager
+           
+def read_employees
+    file = File.read('employee.json')
+    data_hash = JSON.parse(file)
+    employees = Array.new 
+    l = data_hash.length-1
+    for n in 0..l
+        employee = Employee.new(data_hash[n]['first_name'],data_hash[n]['last_name'],data_hash[n]['code'],data_hash[n]['salary'],data_hash[n]['number'])
+        employees.append(employee)
+    end
+    return employees
+end
+def read_employers
+    file = File.read('employer.json')
+    data_hash = JSON.parse(file)
+    employers = Array.new 
+    l = data_hash.length-1
+    for n in 0..l
+        employer = Employee.new(data_hash[n]['first_name'],data_hash[n]['last_name'],data_hash[n]['code'],data_hash[n]['salary'],data_hash[n]['email'])
+        employers.append(employer)
+    end
+    return employers
+end
+
+def salaries
+    employers = read_employers()
+    employees = read_employees()
+    salaries = Array.new
+    for n in 0..employers.length-1
+        salaries.append(employers[n].salary)
+    end
+    for n in 0..employees.length-1
+        salaries.append(employees[n].salary)
+    end
+    return salaries
+end
+
+
+end
+
+class Statistics
+    def average_salary(array)
+        total = 0
+        for i in 0..array.length-1
+            total+= array[i]
+        end
+        return total / array.length
+    end
+    def max_salary(array)
+        max =array[0]
+        for i in 1..array.length-1
+            if max < array[i]
+                max = array[i]
+            end
+        end
+        return max
+    end
+    def min_salary(array)
+        min =array[0]
+        for i in 1..array.length-1
+            if min > array[i]
+                min = array[i]
+            end
+        end
+        return min
+    end
+    def median(array)
+        return array[array.length/2]
+    end
+end
+manager = Manager.new
+statistician = Statistics.new
+puts statistician.average_salary(manager.salaries)
+puts statistician.max_salary(manager.salaries)
+puts statistician.min_salary(manager.salaries)
+puts statistician.median(manager.salaries.sort)
---
+++ test.rb
@@ -0,0 +1,193 @@
+#rb informativo acerca de la sintaxis de ruby
+#Imprimir en consola
+puts "Hello world"
+
+#variables
+puts "variables"
+age = 34
+name = "José Bermudez Blandón"
+course = "Paradigmas de Programacion"
+casado = false
+heigth = 1.73
+puts age.to_s+" "+name+" "+course+" "+casado.to_s+" "+heigth.to_s
+
+#strings
+puts "strings"
+x = "Universidad Nacional de Costa Rica"
+puts x.upcase()
+puts x.downcase()
+puts x.length()
+puts x.include? "Nacional"
+puts x[9]
+puts x[0,10]
+puts x.index("U")
+
+
+#math
+#basic operations
+puts "Basic operations"
+puts 5+3
+puts 2/2
+puts 2**2
+puts 2*3
+puts 5-2
+puts 4%2
+puts 5/2.0
+
+
+puts "some methods"
+num = 20.9
+puts num.ceil()
+puts num.floor()
+puts num.round()
+puts Math.sqrt(20)
+
+
+#input del usuario por consola
+puts "enter you name: "
+name = gets.chomp()
+puts "Age"
+age = gets.chomp()
+puts "Hello "+name+", you are cool"
+
+#arrays
+numbers = Array[1,2,3,4,5,65]
+names = Array["Geancarlo","Frank","Oldemar","Diana"]
+others = Array[numbers,names,"different datatypes"]
+puts numbers
+puts names
+puts others
+
+#array methods
+puts "array methods"
+puts numbers[0]
+puts names[1,2] 
+others[0,2]= "Paradigmas"
+puts others
+courses = Array.new #array vacio sin nada
+courses[0] = 4
+courses[1] = 1
+puts courses
+puts courses.sort()
+puts courses.sort!
+puts courses.reverse
+puts courses.length()
+
+#Hashes    
+states ={
+    :Pennysilvania => "PA",
+    :New_york => "NY",
+    :Oregon => "OR"
+}
+puts states[:New_york]
+
+#funciones
+def sayhi(x="User")
+    puts "Hello "+x
+end
+
+sayhi
+
+def cube(num)
+    return num**3,43
+end
+puts cube(3)
+
+
+#conditionals
+
+male = true
+tall = true
+# and 
+# or
+# !negation
+if male and tall
+    puts "you are a tall male"
+elsif !male
+    put "You are not male"
+else
+    puts "not male or not tall"
+end
+    
+def get_day_name(day)
+    day_name =""
+    case day
+    when "mon"
+        day_name ="Monday"
+    else
+        day_name ="invalid"
+    end
+    return day_name
+end
+
+puts get_day_name("msd")
+
+
+#loops
+
+index =1
+
+while index <= 5
+    puts index
+    index +=1
+end
+
+for element in numbers
+    print(element)
+end
+
+numbers.each do |number|
+    puts numbers
+end
+
+for number in 0..5
+    puts number
+end
+
+6.times do |index|
+    puts index
+end
+
+File.open("personal.txt","r") do |file|
+    for line in file.readlines()
+        puts line
+    end
+end
+
+File.open("personal.txt","a") do |file|
+    file.write("\nOscar, accounting")
+end
+
+l = Array[1,2,3,4,5]
+begin
+    num = 10 /0
+    l["numero"]
+rescue ZeroDivisionError => e
+    puts e
+rescue TypeError => e
+    puts e
+end
+
+#Objects
+class Book
+    attr_accessor :title, :author, :pages
+    def initialize(title,author,pages)
+        @title = title
+        @author  = author
+        @pages = pages
+    end
+    def is_long
+        if @pages>300
+            return true
+        end
+            return false
+    end
+end
+book1 = Book.new("Harry Potter","JK rowling",400)
+puts book1.is_long
+
+class HorrorBook < Book
+   
+end
+hbook = HorrorBook.new("Nun","Geancarlo",700)
+puts hbook.is_long()
